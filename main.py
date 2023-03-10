586827aa747f841ce434e0c4b2ad6b09473fcc59
Condesay <39847380+condesay@users.noreply.github.com>
2023-03-10 12:16:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,71 +1,49 @@
-# import streamlit as st
-
-# st.title("ChatGPT-like Web App")
-# #storing the chat
-# if 'generated' not in st.session_state:
-#     st.session_state['generated'] = []
-# if 'past' not in st.session_state:
-#     st.session_state['past'] = []
-# user_input=st.text_input("You:",key='input')
-# if user_input:
-#     output=generate_response(user_input)
-#     #store the output
-#     st.session_state['past'].append(user_input)
-#     st.session_state['generated'].append(output)
-# if st.session_state['generated']:
-#     for i in range(len(st.session_state['generated'])-1, -1, -1):
-#         message(st.session_state["generated"][i], key=str(i))
-#         message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
-
-
-
-
+import openai
 import streamlit as st
+from streamlit_chat import message
 
-# this loop will let us ask questions continuously
-
-while True:
-    
-    # Set up the model and prompt
-    model_engine = "text-davinci-003"
-    
-    prompt = input('Enter new prompt: ')
-
-    if 'exit' in prompt or 'quit' in prompt:
-        break
-
-    # Generate a response
-    # given the most recent context (4096 characters)
-    # continue the text up to 2048 tokens ~ 8192 charaters
-    completion = openai.Completion.create(
-        engine=model_engine,
+def generate_response(prompt, api_key):
+    openai.api_key = api_key
+    completion=openai.Completion.create(
+        engine='text-davinci-003',
         prompt=prompt,
         max_tokens=1024,
         n=1,
         stop=None,
-        temperature=0.5,
+        temperature=0.3,
     )
-    
-    # extracting useful part of response
-    response = completion.choices[0].text
-    
-    # printing response
-    print(response)
 
-    
-st.title("ChatGPT-like Web App")
-#storing the chat
-if 'generated' not in st.session_state:
-    st.session_state['generated'] = []
-if 'past' not in st.session_state:
-    st.session_state['past'] = []
-user_input=st.text_input("You:",key='input')
-if user_input:
-    output=generate_response(user_input)
-    #store the output
-    st.session_state['past'].append(user_input)
-    st.session_state['generated'].append(output)
-if st.session_state['generated']:
-    for i in range(len(st.session_state['generated'])-1, -1, -1):
-        message(st.session_state["generated"][i], key=str(i))
-        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+    message=completion.choices[0].text
+    return message
+
+def main():
+    st.title("ChatGPT Web App")
+
+    # get user API key
+    api_key = st.text_input("Enter OpenAI API Key:", type="password")
+
+    if api_key:
+        # storing the chat
+        if 'generated' not in st.session_state:
+            st.session_state['generated'] = []
+
+        if 'past' not in st.session_state:
+            st.session_state['past'] = []
+
+        user_input=st.text_input("You:",key='input')
+
+        if user_input:
+            output=generate_response(user_input, api_key)
+
+            #store the output
+            st.session_state['past'].append(user_input)
+            st.session_state['generated'].append(output)
+
+        if st.session_state['generated']:
+
+            for i in range(len(st.session_state['generated'])-1, -1, -1):
+                message(st.session_state["generated"][i], key=str(i))
+                message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+
+if __name__ == '__main__':
+    main()
